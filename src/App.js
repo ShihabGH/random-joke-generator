342f999c820306967f386cdf13c7ab1ebfbee35d
Shihab183 <shihabmahmoodofficial@gmail.com>
2023-06-29 11:28:20 -0500

    fnished fetching data from API and making design changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import "./App.scss"
 
 function App() {
+  const [jokes, setJokes] = useState([]);
+
+  const getJokes = async () => {
+    const options = {
+      method: 'GET',
+      url: 'https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes',
+      headers: {
+        'X-RapidAPI-Key': '6f39753bd7msh189ff5c29f0bf09p11acecjsn013739bef035',
+        'X-RapidAPI-Host': 'jokes-by-api-ninjas.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await axios.request(options);
+      setJokes(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1 >Random Joke Generator:</h1>
+        {jokes.map((jokeObj, index) => (
+          <p key={index}>{jokeObj.joke}</p>
+        ))}
+        <button onClick={getJokes}>Get new jokes</button>
+      </div>
     </div>
   );
 }
